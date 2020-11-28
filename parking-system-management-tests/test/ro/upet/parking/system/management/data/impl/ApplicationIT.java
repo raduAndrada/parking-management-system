4026f98c850a317a1ac6806843db0a56172a1b6b
radu <radu.andrada8@gmail.com>
2020-11-28 22:47:26 +0200

    feat: added test configurations

---
+++ parking-system-management-tests/test/ro/upet/parking/system/management/data/impl/ApplicationIT.java
@@ -0,0 +1,38 @@
+package ro.upet.parking.system.management.data.impl;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import ro.upet.parking.system.management.rest.Application;
+
+// JUnit 5 example with Spring Boot >= 2.2.6
+@Testcontainers
+@SpringBootTest(classes= Application.class)
+public class ApplicationIT {
+
+  @Container
+  public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+    .withPassword("inmemory")
+    .withUsername("inmemory");
+
+  @DynamicPropertySource
+  static void postgresqlProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+  }
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
+
+
