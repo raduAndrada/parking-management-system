8c9ccfec9b655e286d31bd0979efa819b7f98ab0
radu <radu.andrada8@gmail.com>
2020-12-07 21:55:36 +0200

    fix: vehicle repository query error

--- parking-system-management-data-impl/src/ro/upet/parking/system/management/data/impl/vehicle/VehicleRepository.java
+++ parking-system-management-data-impl/src/ro/upet/parking/system/management/data/impl/vehicle/VehicleRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ro.upet.parking.system.management.data.api.vehicle.VehicleEntity;
@@ -13,6 +14,7 @@ public interface VehicleRepository extends JpaRepository<VehicleEntity, Long>{
 	
 	public Optional<VehicleEntity> findOptionalByLicencePlate(final String licencePlate);
 	
+	@Query("SELECT v FROM VehicleEntity v WHERE v.user.username = ?1")
 	public List<VehicleEntity> findAllByUserUsename(final String username);
 
 }
